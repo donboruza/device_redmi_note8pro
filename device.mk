cc91aa54326d8f2f0b8b77b436749a52a7e8636a
bengris32 <bengris32@protonmail.ch>
2022-07-20 23:37:58 +0530

    begonia: Introduce MtkInCallService
    * My attempt at fixing in call gain control on MediaTek devices.
    * It attempts to replicate what the stock MediaTek framework does
      when controlling the volume of the earpiece speaker, since MTK's
      audio HAL is not able to set the gain of the speaker from the
      values sent by AOSP framework.
    
    Change-Id: I43f727f6fb73a0d38457a41a8361653d1d00e220

--- device.mk
+++ device.mk
@@ -270,6 +270,10 @@ PRODUCT_COPY_FILES += \
     frameworks/av/media/libstagefright/data/media_codecs_google_c2_audio.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_c2_audio.xml \
     frameworks/av/media/libstagefright/data/media_codecs_google_c2_video.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_c2_video.xml
 
+# MtkInCallService
+PRODUCT_PACKAGES += \
+    MtkInCallService
+
 # Neural
 PRODUCT_PACKAGES += \
     android.hardware.neuralnetworks@1.1.vendor \
